7c45799e8e32d2679c67179b244be3233acd7510
YH J <davidj@YHui-MacBookAir.local>
2021-12-09 23:41:20 +0900

    웹드라이버 설치 URL

---
+++ setupForM1.sh
@@ -0,0 +1,6 @@
+mkdir ~/webdriver
+cd ~/webdriver
+curl -o temp.zip https://chromedriver.storage.googleapis.com/97.0.4692.36/chromedriver_mac64_m1.zip
+unzip temp.zip
+rm temp.zip
+echo "크롬 웹 드라이버 macOS M1 설치완료"
---
+++ setupForMac.sh
@@ -0,0 +1,6 @@
+mkdir ~/webdriver
+cd ~/webdriver
+curl -o temp.zip https://chromedriver.storage.googleapis.com/97.0.4692.36/chromedriver_mac64.zip
+unzip temp.zip
+rm temp.zip
+echo "크롬 웹 드라이버 macOS 설치완료"
