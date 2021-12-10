dcd058368751a4feda47917ab93502f9a70abe2b
davidjung-kr <jerryjueng@daum.net>
2021-12-11 01:05:54 +0900

    불필요한 쉘파일 삭제

--- setupForMac.sh
+++
@@ -1,6 +0,0 @@
-mkdir ~/webdriver
-cd ~/webdriver
-curl -o temp.zip https://chromedriver.storage.googleapis.com/97.0.4692.36/chromedriver_mac64.zip
-unzip temp.zip
-rm temp.zip
-echo "크롬 웹 드라이버 macOS 설치완료"
